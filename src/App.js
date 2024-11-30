bfd084f080cfbe18d6c9588f693a4539883d6f9c
Shivani L N <lnshivani999@gmail.com>
2024-11-30 22:38:07 +0530

    Fix routing issue with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './pages/Navbar';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
